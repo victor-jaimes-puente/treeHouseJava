ee8ce755ae06c70a414ba4db1cb11bb1c1b2b073
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-09 23:28:24 -0500

    starting practice

---
+++ JavaBasics/src/Greeter.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Greeter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // TODO:  1. Create a new String named place and assign it a value of your choosing.
+
+        // TODO:  2. Using the provided console object,
+        //           prompt the user "What is your first name?" and store that in a variable
+
+        // TODO:  3. Print out to the console "Hello <FIRST_NAME>!  Welcome to <PLACE>."
+
+    }
+
+
+}
+
