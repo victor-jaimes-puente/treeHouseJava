03841f6affd8e9b5cb97bb7eb6d3240bb6f984e6
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-10 16:38:21 -0500

    created pg 13 filter

--- JavaBasics/src/TreeStory.java
+++ JavaBasics/src/TreeStory.java
@@ -6,22 +6,31 @@ public class TreeStory {
 
         Scanner scanner = new Scanner(System.in);
 //        System.out.println("Enter firstName");
-//        String firstName = scanner.nextLine();
-//        System.out.printf("%s is learning Java", firstName);
-        System.out.println();
-        System.out.println("Enter a noun ");
-        String noun = scanner.nextLine();
-        System.out.println();
-        System.out.println("Enter an adjective ");
-        String adjective = scanner.nextLine();
-        System.out.println();
-        System.out.println("Enter a verb ");
-        String verb = scanner.nextLine();
-        System.out.println();
-        System.out.println("Enter an adverb ");
-        String adverb = scanner.nextLine();
+////        String firstName = scanner.nextLine();
+////        System.out.printf("%s is learning Java", firstName);
+//        System.out.println();
+//        System.out.println("Enter a noun ");
+//        String noun = scanner.nextLine();
+//        System.out.println();
+//        System.out.println("Enter an adjective ");
+//        String adjective = scanner.nextLine();
+//        System.out.println();
+//        System.out.println("Enter a verb ");
+//        String verb = scanner.nextLine();
+//        System.out.println();
+//        System.out.println("Enter an adverb ");
+//        String adverb = scanner.nextLine();
+//
+//        System.out.printf("A %s %s was %s %s today.", adjective,noun,verb,adverb);
+        System.out.println("What is your age?");
+        int age = scanner.nextInt();
 
-        System.out.printf("A %s %s was %s %s today.", adjective,noun,verb,adverb);
+        if (age < 13){
+            //insert exit code
+            System.out.println();
+            System.out.println("Must me atleast 13 to use this program!");
+            System.exit(0);
+        }
 
     }
 
