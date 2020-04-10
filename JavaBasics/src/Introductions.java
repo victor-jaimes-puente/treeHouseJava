3cc72e80d729e02a86baeb136b08d38dc4461f92
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-09 22:20:32 -0500

    updating tree

---
+++ JavaBasics/src/Introductions.java
@@ -0,0 +1,14 @@
+import java.io.Console;
+
+public class Introductions {
+
+    public static void main(String[] args) {
+        Console console = System.console();
+        // Welcome to the Introductions program!  Your code goes below here
+        String firstName = "Victor";
+        System.out.print("\n Hello, my name is Vic\n");
+        System.out.print("\n Vic is learning Java\n\n");
+
+        System.out.printf("\n %s is learning Java \n\n", firstName);
+    }
+}
