7e68fcadb0887abf9ef9d35a231d8bdfa4296240
VictorJP <victor.jaimes.puente@gmail.com>
2020-04-09 23:06:57 -0500

    multiple stirngs

--- JavaBasics/src/TreeStory.java
+++ JavaBasics/src/TreeStory.java
@@ -4,9 +4,18 @@ public class TreeStory {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter firstName");
-        String firstName = scanner.nextLine();
-        System.out.printf("%s is learning Java", firstName);
+//        System.out.println("Enter firstName");
+//        String firstName = scanner.nextLine();
+//        System.out.printf("%s is learning Java", firstName);
+        System.out.println();
+        System.out.println("Enter a noun");
+        String noun = scanner.nextLine();
+        System.out.println();
+        System.out.println("Enter an adjective");
+        String adjective = scanner.nextLine();
+
+        System.out.printf("A %s %s", adjective, noun);
+
     }
 
 }
